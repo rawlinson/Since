388b9c4d3059bd772ffd6625a251c37850b55295
Tim Rawlinson <ekte.navn@gmail.com>
2016-04-15 20:44:30 +0100

    Added NuGet package for Since.Versioning and removed VSIX.

---
+++ Since.Versioning/Since.Versioning/Tools/install.ps1
@@ -0,0 +1,7 @@
+﻿param($installPath, $toolsPath, $package, $project)
+$item = $project.ProjectItems.Item("Properties\\AssemblyVersionInfo.tt")
+$item.Properties.Item("BuildAction").Value = [int]0
+$item.Properties.Item("CustomTool").Value = "TextTemplatingFileGenerator"
+$item.Properties.Item("CopyToOutputDirectory").Value = [int]2
+$item.Properties.Item("ItemType").Value = "None"
+$item.Properties.Item("BuildAction").Value = [int]0
